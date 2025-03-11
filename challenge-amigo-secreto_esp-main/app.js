ecbec346a0f28ae101ebc2930324fef041cd7d9b
lean77777 <87152636+lean77777@users.noreply.github.com>
2025-03-11 20:00:08 -0300

    Actualiza el app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -6,6 +6,9 @@ const ulResultado = document.getElementById("resultado");
 function agregarAmigo() {
     listaAmigos.push (inputAmigo.value);
     ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+    if(inputAmigo.value == 0) {
+        alert('Por favor, inserte un nombre.')
+    }
 }
 
 function sortearAmigo() {
